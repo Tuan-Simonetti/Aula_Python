6899a63c1bddb1688881c5b3107b4316694b131e
Tuan-Simonetti <tuan.gomes@picpay.com>
2020-08-26 20:00:56 -0300

    Aula_01

---
+++ Aulas/Aula_1.py
@@ -0,0 +1,12 @@
+print("arroz", "feijão", "bife", "batata frita")
+print("Olá, mundo!")
+print('Olá, mundo!')
+print("""
+Aqui, eu consigo escrever um parágrafo.
+
+Consigo separar de uma forma mais adequada ao português.
+
+Joia?
+""")
+
+print("Aqui, eu consigo escrever um parágrafo. \nConsigo separar de uma forma mais adequada ao português.\n\nJoia?")
