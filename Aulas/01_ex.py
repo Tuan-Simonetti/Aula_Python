7fe94d05cbe68be86f463d910e5ec753ec182cc1
Tuan-Simonetti <tuan.gomes@picpay.com>
2020-08-29 15:03:40 -0300

    Adiciona o exercicio 1 e a aula_3

---
+++ Aulas/01_ex.py
@@ -0,0 +1,7 @@
+nome = str(input('Nome: '))
+idade = int(input('Idade: '))
+email = str(input('E-mail: '))
+telefone = str(input('Telefone[(XX) X.XXXX-XXXX]: '))
+telefone = int(input('Telefone: '))
+print(nome, idade, email, telefone)
+print(nome,"\n", idade,"\n", email,"\n", telefone)
---
+++ Aulas/Aula_3.py
@@ -0,0 +1,62 @@
+# Operadores Aritmeticos
+
+soma = "+"
+subt = "-"
+mult = "*"
+div = "/"
+exponenciacao = "**"
+parte_inteira = "//"
+
+soma_exe = 2 + 2        #= 4 ok
+subt_exe = 2 - 2        #= 0 ok
+mult_exe = 2 * 2        #= 4 ok
+div_exe = 2 / 2         #= 1 ok
+expo_exe = 2 ** 2       #= 4 ok
+parte_inteira_exe = 2 // 2   #= 1  ok
+
+print(soma_exe, "\n", subt_exe, "\n", mult_exe, "\n", div_exe, "\n", expo_exe, "\n", parte_inteira_exe)
+
+
+
+
+
+soma_2 = 2 + 2
+texto = f'O resultado da soma é: {soma_2}'
+
+#print(nome, idade, email, telefone)
+print('O resultado da soma é: ', (2 + 2))
+print('O resultado da soma é: ', soma_2)
+print(texto, soma_2)
+print('O resultado da soma é: ' + str(soma_2))
+print(texto + str(soma_2))
+print(texto)
+
+
+
+print('texto', 2 + 2, 8 / 2)
+x = (2 + 2)*2
+
+
+float = 2.44
+soma = (int(input('Primeiro Operador: ')) + int(input('Segundo Operador: ')))
+print(f'Resultado: {soma}')
+
+soma1 = int(input('Primeiro Operador: '))
+soma2 = int(input('Segundo Operador: '))
+resultado = (soma1 + soma2)
+print(f'Resultado: {resultado}')
+
+
+
+
+
+pais = "brasil"
+ano = "1500"
+descobridor = "Cabral"
+
+
+#'No ano de ANO, o PAIS foi descoberto por DESCOBRIDOR'
+#f'No ano de {ANO}, o {PAIS} foi descoberto por {DESCOBRIDOR}'
+
+print(f"Prova dos 9: {int(input('Informe um int'))}")
+print(f"Prova dos 9: {float(input('Informe um int'))}")
